9ef6c924887a671e3e6bd8ddcf29c3d282743d82
Luigui Marco <luigui.lupacca@tecsup.edu.pe>
2022-11-01 18:04:08 -0500

    relacioneslab10

---
+++ app/Models/Comentario.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Comentario extends Model
+{
+    use HasFactory;
+    public function User()
+        {
+            return $this->belongsTo(User::class);
+        }
+    public function Fotos()
+        {
+            return $this->belongsTo(Foto::class);
+        }
+}
